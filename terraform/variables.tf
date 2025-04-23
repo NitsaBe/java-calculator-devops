3acdccdd8c3817fb859df2e6509fffe09505fd44
unknown <bedianashvili.nitsa@kiu.edu.ge>
2025-04-23 22:52:24 +0400

    added a readme

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,3 +1,9 @@
+variable "base_directory" {
+  description = "Base directory for all deployment artifacts"
+  type        = string
+  default     = "terraform"
+}
+
 variable "app_name" {
   description = "Name of the application"
   type        = string
@@ -20,4 +26,10 @@ variable "production_port" {
   description = "Port for production environment"
   type        = number
   default     = 8080
+}
+
+variable "health_check_interval" {
+  description = "Interval in seconds between health checks"
+  type        = number
+  default     = 60
 }
